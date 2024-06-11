e3f9a90edb33e0f75df45d40f07fcb4e3ee51257
SergeyTrue <belousov.sd@gmail.com>
2024-06-11 07:16:34 +0300

    excercise1 added

---
+++ 2Variables/excercise2/excercise2.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	tempconv "DonovanKeringanExcercises/2Variables/excercise1"
+	"flag"
+	"fmt"
+	"os"
+)
+
+var from = flag.String("from", "", "from convert")
+var to = flag.String("to", "", "convert to")
+var temp = flag.Float64("temp", 0, "temperature")
+
+func main() {
+	flag.Parse()
+	if len(os.Args) > 1 {
+		if *from == "c" && *to == "f" {
+			fmt.Printf("Вот мы и законвертили цельсий в файренгейт: %.2f\n", tempconv.CToF(tempconv.Celsius(*temp)))
+		} else {
+			fmt.Println("No arguments have been provided")
+		}
+	}
+}
