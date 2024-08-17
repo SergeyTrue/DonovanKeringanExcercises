b005a61d183f2c42d94660cb67424231a18fb4b9
SergeyTrue <belikk@ya.ru>
2024-08-17 09:52:08 +0300

    mandelbrot colorized

--- 3BasicDataTypes/Excercise_3_5/Mandelbrot_RGBA.go
+++ 3BasicDataTypes/Excercise_3_5/Mandelbrot_RGBA.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/crazy3lf/colorconv"
 	"image"
 	"image/color"
 	"image/png"
@@ -12,7 +13,7 @@ import (
 func main() {
 	const (
 		xmin, ymin, xmax, ymax = -2, -2, +2, +2
-		width, height          = 1024, 1024
+		width, height          = 10000, 10000
 	)
 	f, err := os.Create("mandelbrot.png")
 	if err != nil {
@@ -21,7 +22,7 @@ func main() {
 	defer f.Close()
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
 	for py := 0; py < height; py++ {
-		y := float64(py)/width*(ymax-ymin) + ymin
+		y := float64(py)/height*(ymax-ymin) + ymin
 		for px := 0; px < width; px++ {
 			x := float64(px)/width*(xmax-xmin) + xmin
 			z := complex(x, y)
@@ -33,13 +34,20 @@ func main() {
 }
 
 func mandelbrot(z complex128) color.Color {
-	const iterations = 200
-	const contrast = 15
+	const iterations = 100
+	const contrast = 250
 	var v complex128
 	for n := uint8(0); n < iterations; n++ {
 		v = v*v + z
 		if cmplx.Abs(v) > 2 {
-			return color.RGBA{R: 255 - contrast*n, G: 0, B: 0, A: 255}
+			h := float64(n%6) * 360. / 64
+			s := 1.0
+			v := 1 - float64(n)/400
+			r, g, b, err := colorconv.HSVToRGB(h, s, v)
+			if err != nil {
+				log.Fatal(err)
+			}
+			return color.RGBA{r, g, b, contrast}
 		}
 	}
 	return color.Black
